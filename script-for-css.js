71e5c15c73c22969764292a2760f84cf14576b3a
renzogabrielsy <renzogabrielsy@gmail.com>
2021-06-01 20:58:24 +0800

    Site update
    -still dont know how to do decent version control (i will get to that i promise)
    -fixed navbar bugs
    -cleaned up some formatting in the home page
    -added a js file for DOM manipulation (didnt need it in the end as navbar bugs were fixed via css/bootstrap)

---
+++ script-for-css.js
@@ -0,0 +1,13 @@
+// const navbarToggler = document.querySelector('.navbar-toggler');
+// const topNavbar = document.querySelector('#topnavbar')
+// const toggleStatus = document.querySelector('#topnavbar button')
+
+// navbarToggler.onclick = function () {
+
+//     if (toggleStatus.className != 'navbar-toggler') {
+//         topNavbar.style.backgroundColor = '#a980add7'
+//     }
+//     else {
+//         topNavbar.style.backgroundColor = ''
+//     }
+// }
